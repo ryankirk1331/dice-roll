02353fb11b81ca77f3545366f20470dc64c77c53
Ryan <ryankirk1331@gmail.com>
2020-11-12 13:28:24 -0500

    project complete

---
+++ src/Die.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import './Die.css'
+
+class Die extends Component {
+    render() {
+        
+        return(
+            <div>
+                <i className= {`Die fas fa-dice-${this.props.face} ${this.props.rolling 
+                    ? 'shaking' : '' }`} />
+            </div> 
+        );
+    }
+}
+
+export default Die;
